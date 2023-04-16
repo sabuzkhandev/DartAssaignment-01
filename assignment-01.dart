6795f597ce2bf98264130f3fca9b52d3fa919e5f
SABUZ KHAN <95304693+sabuzkhandev@users.noreply.github.com>
2023-04-16 08:47:56 +0600

    question add

--- assignment-01.dart
+++ assignment-01.dart
@@ -1,3 +1,8 @@
+//Take a list of phone numbers example
+//phoneNumber = [ +88, 01768131685, 01768171985, 01768111286, 01768131685]
+//And write program that prints out the full number like 
+//‘+8801768171985’ You have to print out all numbers given in the list except +88.
+
 main(){
   // List of the phoneNumber
  var phoneNumber = [ "+88", "01768131685", "01768171985", "01768111286", "01768131685"];
